9547f6cafb00466a3244cc1f41f4561d25ccd79b
ernestmucheru <81610268+ernestmucheru@users.noreply.github.com>
2021-05-26 08:35:14 +0300

    Add files via upload

---
+++ src/app/services/userservices.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { User } from '../user';
+import { Repository } from '../repository';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserserviceService {
+
+  foundUser: User;
+  allRepos: Repository;
+ 
+  constructor(private http: HttpClient) { 
+    this.foundUser = new User("","","","",0,0,0,"",new Date);
+    this.allRepos = new Repository("","","",new Date,0,0,"");
+  }
+  searchUSer(searchName: string){
+    interface Responce {
+      url:string,
+      login: string;
+      html_url:string;
+      location:string
+      public_repos:number;
+      followers:number;
+      following:number;
+      avatar_url:string;
+      created_at:Date;
+    }
+    return new Promise<void>((resolve) => {
+      this.http.get<Responce>('https://api.github.com/users/'+searchName+'?=token'+ this.http).toPromise().then(
+        (result) => {
+          this.foundUser = result;
+          console.log(result);
+          resolve();
+        },
+        
+      );
+    })
+  }
+  getRepos(searchName){
+    interface Repos{
+      name:string;
+      html_url:string;
+      forks:number;
+      watchers_count:number;
+      description:string;
+      language:string;
+      created_at:Date;
+    }
+    return new Promise<void>((resolve,reject)=>{
+      this.http.get<Repos>('https://api.github.com/users/'+searchName+"/repos?order=created&sort=asc?access_token="+environment).toPromise().then(
+        (results) => {
+          this.allRepos = results;
+          resolve();
+        },
+        
+      );
+    });
+  }
+  }
+    
